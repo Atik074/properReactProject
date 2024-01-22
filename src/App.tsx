c5e705ee4890a77f8a264fa0956138ba158465ff
Atik074 <atiksmats@gmail.com>
2024-01-22 22:56:25 +0600

    variant button by resuable button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import MainLayout from "./component/layout/MainLayout"
+import Button from "./ui/Button"
+
 
 
 function App() {
 
   return (
     <>
-      <MainLayout/>
+       <MainLayout/>
+
+    
+        <div>
+        <Button className="btn   p-4 m-16 text-2xl rounded-lg w-[500px]"  variant='outline'></Button>
+      
+        </div>
+
     </>
   )
 }
---
+++ src/ui/Button.tsx
@@ -0,0 +1,26 @@
+
+import cn from '../utils/cn';
+
+
+const Button = ({className, variant}) => {
+
+    const getVariant = (variant)=>{
+        switch (variant) {
+            case "outline":
+                return "btn-outline"
+                
+              case 'ghost' :
+                return "btn-ghost"
+        
+            default:
+             return "btn-solid";
+        }
+    }
+
+
+    return <button className={cn(
+        getVariant(variant) ,className
+    )}>Click</button>
+};
+
+export default Button;
